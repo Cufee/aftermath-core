70500ead4c820665bf9e153e9604f5b3cd5a7826
Cufee <vkouzin@outlook.com>
2024-01-18 15:41:46 -0500

    more and better types

--- types/dataprep.go
+++ types/dataprep.go
@@ -1,5 +1,16 @@
 package types
 
-import "github.com/cufee/aftermath-core/internal/logic/server/handlers/stats"
+import (
+	"github.com/cufee/aftermath-core/internal/core/server"
+	"github.com/cufee/aftermath-core/internal/logic/server/handlers/stats"
+	"github.com/cufee/aftermath-core/internal/logic/users"
+	wg "github.com/cufee/am-wg-proxy-next/types"
+)
 
-type StatsResponse stats.SessionStatsResponse
+type AccountsSearchResponse server.Response[wg.Account]
+
+type RenderSessionResponse server.Response[string]
+
+type StatsSessionResponse server.Response[stats.SessionStatsResponse]
+
+type UsersConnectionResponse server.Response[users.UserConnection]
