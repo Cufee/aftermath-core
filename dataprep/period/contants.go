f33b1eb4498b3aeefd71f299e426b1c28685c48a
Cufee <vkouzin@outlook.com>
2024-03-04 12:56:20 -0500

    removed comments

--- dataprep/period/contants.go
+++ dataprep/period/contants.go
@@ -2,7 +2,5 @@ package period
 
 import "github.com/cufee/aftermath-core/dataprep"
 
-// var DefaultBlocks = [][]dataprep.Tag{{dataprep.TagWN8}, {dataprep.TagBattles, dataprep.TagWinrate}, {dataprep.TagDamageRatio, dataprep.TagAvgDamage, dataprep.TagAccuracy}}
-// var DefaultBlocks = [][]dataprep.Tag{{dataprep.TagBattles, dataprep.TagWinrate, dataprep.TagAvgTier}, {dataprep.TagDamageRatio, dataprep.TagAvgDamage, dataprep.TagAccuracy}}
 var DefaultBlocks = [][]dataprep.Tag{{dataprep.TagAvgDamage, dataprep.TagDamageRatio, dataprep.TagAvgTier}, {dataprep.TagBattles, dataprep.TagWN8}, {dataprep.TagWinrate, dataprep.TagAccuracy, dataprep.TagSurvivalPercent}}
 var DefaultHighlights = []highlight{HighlightWN8, HighlightAvgDamage, HighlightBattles}
