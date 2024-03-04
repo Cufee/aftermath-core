1f54fa82723df577796aaa90b7b4db44722790e1
Cufee <vkouzin@outlook.com>
2024-03-04 12:57:10 -0500

    moved sorting around

--- dataprep/period/contants.go
+++ dataprep/period/contants.go
@@ -3,4 +3,4 @@ package period
 import "github.com/cufee/aftermath-core/dataprep"
 
 var DefaultBlocks = [][]dataprep.Tag{{dataprep.TagAvgDamage, dataprep.TagDamageRatio, dataprep.TagAvgTier}, {dataprep.TagBattles, dataprep.TagWN8}, {dataprep.TagWinrate, dataprep.TagAccuracy, dataprep.TagSurvivalPercent}}
-var DefaultHighlights = []highlight{HighlightWN8, HighlightAvgDamage, HighlightBattles}
+var DefaultHighlights = []highlight{HighlightBattles, HighlightWN8, HighlightAvgDamage}
